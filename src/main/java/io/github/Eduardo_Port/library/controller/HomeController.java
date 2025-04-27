d77102a357c606fd918b1818f26271ad49a890d1
Eduardo <edu.friv10@gmail.com>
2025-04-27 19:50:25 -0300

    "first commit"

---
+++ src/main/java/io/github/Eduardo_Port/library/controller/HomeController.java
@@ -0,0 +1,13 @@
+package io.github.Eduardo_Port.library.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
